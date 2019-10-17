922ee285f36900e8aa44b6e77d3936a049364069
Kenny Kerr <kenny.kerr@microsoft.com>
2019-10-17 13:58:37 -0700

    tests

--- TestComponent/TestRunner.cpp
+++ TestComponent/TestRunner.cpp
@@ -25,65 +25,65 @@ namespace winrt::TestComponent::implementation
             return m_counter * 100 / m_total;
         }
 
-        void T1_Simple()
+        void Simple()
         {
         }
-        bool T2_Bool(bool a, bool& b)
-        {
-            b = a;
-            return a;
-        }
-        uint8_t T3_UInt8(uint8_t a, uint8_t& b)
+        bool Params_Bool(bool a, bool& b)
         {
             b = a;
             return a;
         }
-        uint16_t T4_UInt16(uint16_t a, uint16_t& b)
+        uint8_t Params_UInt8(uint8_t a, uint8_t& b)
         {
             b = a;
             return a;
         }
-        uint32_t T5_UInt32(uint32_t a, uint32_t& b)
+        uint16_t Params_UInt16(uint16_t a, uint16_t& b)
         {
             b = a;
             return a;
         }
-        uint64_t T6_UInt64(uint64_t a, uint64_t& b)
+        uint32_t Params_UInt32(uint32_t a, uint32_t& b)
         {
             b = a;
             return a;
         }
-        int16_t T7_Int16(int16_t a, int16_t& b)
+        uint64_t Params_UInt64(uint64_t a, uint64_t& b)
         {
             b = a;
             return a;
         }
-        int32_t T8_Int32(int32_t a, int32_t& b)
+        int16_t Params_Int16(int16_t a, int16_t& b)
         {
             b = a;
             return a;
         }
-        int64_t T9_Int64(int64_t a, int64_t& b)
+        int32_t Params_Int32(int32_t a, int32_t& b)
         {
             b = a;
             return a;
         }
-        float T10_Single(float a, float& b)
+        int64_t Params_Int64(int64_t a, int64_t& b)
         {
             b = a;
             return a;
         }
-        double T11_Double(double a, double& b)
+        float Params_Single(float a, float& b)
         {
             b = a;
             return a;
         }
-        char16_t T12_Char(char16_t a, char16_t& b)
+        double Params_Double(double a, double& b)
         {
             b = a;
             return a;
         }
-        hstring T13_String(hstring const& a, hstring& b)
+        char16_t Params_Char(char16_t a, char16_t& b)
+        {
+            b = a;
+            return a;
+        }
+        hstring Params_String(hstring const& a, hstring& b)
         {
             b = a;
             return a;
@@ -92,78 +92,78 @@ namespace winrt::TestComponent::implementation
 
     void RunTests(ITests const& tests)
     {
-        tests.T1_Simple();
+        tests.Simple();
         {
             bool const a = true;
             bool b;
-            bool c = tests.T2_Bool(true, b);
-            TEST_REQUIRE(L"T2_Bool", b && c);
+            bool c = tests.Params_Bool(true, b);
+            TEST_REQUIRE(L"Params_Bool", b && c);
         }
         {
             uint8_t a = 123;
             uint8_t b;
-            uint8_t c = tests.T3_UInt8(a, b);
-            TEST_REQUIRE(L"T3_UInt8", a == b && a == c);
+            uint8_t c = tests.Params_UInt8(a, b);
+            TEST_REQUIRE(L"Params_UInt8", a == b && a == c);
         }
         {
             uint16_t a = 123;
             uint16_t b;
-            uint16_t c = tests.T4_UInt16(a, b);
-            TEST_REQUIRE(L"T4_UInt16", a == b && a == c);
+            uint16_t c = tests.Params_UInt16(a, b);
+            TEST_REQUIRE(L"Params_UInt16", a == b && a == c);
         }
         {
             uint32_t a = 123;
             uint32_t b;
-            uint32_t c = tests.T5_UInt32(a, b);
-            TEST_REQUIRE(L"T5_UInt32", a == b && a == c);
+            uint32_t c = tests.Params_UInt32(a, b);
+            TEST_REQUIRE(L"Params_UInt32", a == b && a == c);
         }
         {
             uint64_t a = 123;
             uint64_t b;
-            uint64_t c = tests.T6_UInt64(a, b);
-            TEST_REQUIRE(L"T6_UInt64", a == b && a == c);
+            uint64_t c = tests.Params_UInt64(a, b);
+            TEST_REQUIRE(L"Params_UInt64", a == b && a == c);
         }
         {
             int16_t a = 123;
             int16_t b;
-            int16_t c = tests.T7_Int16(a, b);
-            TEST_REQUIRE(L"T7_Int16", a == b && a == c);
+            int16_t c = tests.Params_Int16(a, b);
+            TEST_REQUIRE(L"Params_Int16", a == b && a == c);
         }
         {
             int32_t a = 123;
             int32_t b;
-            int32_t c = tests.T8_Int32(a, b);
-            TEST_REQUIRE(L"T8_Int32", a == b && a == c);
+            int32_t c = tests.Params_Int32(a, b);
+            TEST_REQUIRE(L"Params_Int32", a == b && a == c);
         }
         {
             int64_t a = 123;
             int64_t b;
-            int64_t c = tests.T9_Int64(a, b);
-            TEST_REQUIRE(L"T9_Int64", a == b && a == c);
+            int64_t c = tests.Params_Int64(a, b);
+            TEST_REQUIRE(L"Params_Int64", a == b && a == c);
         }
         {
             float a = 12.3f;
             float b;
-            float c = tests.T10_Single(a, b);
-            TEST_REQUIRE(L"T10_Single", a == b && a == c);
+            float c = tests.Params_Single(a, b);
+            TEST_REQUIRE(L"Params_Single", a == b && a == c);
         }
         {
             double a = 12.3;
             double b;
-            double c = tests.T11_Double(a, b);
-            TEST_REQUIRE(L"T11_Double", a == b && a == c);
+            double c = tests.Params_Double(a, b);
+            TEST_REQUIRE(L"Params_Double", a == b && a == c);
         }
         {
             char16_t a = L'W';
             char16_t b;
-            char16_t c = tests.T12_Char(a, b);
-            TEST_REQUIRE(L"T12_Char", a == b && a == c);
+            char16_t c = tests.Params_Char(a, b);
+            TEST_REQUIRE(L"Params_Char", a == b && a == c);
         }
         {
             hstring a = L"WinRT";
             hstring b;
-            hstring c = tests.T13_String(a, b);
-            TEST_REQUIRE(L"T13_String", a == b && a == c);
+            hstring c = tests.Params_String(a, b);
+            TEST_REQUIRE(L"Params_String", a == b && a == c);
         }
     }
 
@@ -194,7 +194,7 @@ namespace winrt::TestComponent::implementation
 
         percentage = TestRunner::TestCaller([](ITests const& tests)
             {
-                tests.T1_Simple();
+                tests.Simple();
             });
 
         TEST_REQUIRE(L"TestSelf", percentage > 0 && percentage < 100);
