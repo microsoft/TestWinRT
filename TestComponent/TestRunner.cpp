d32f1d9a80d5234ff06faa12e3c660882187e5c6
Kenny Kerr <kekerr@microsoft.com>
2020-02-10 07:29:04 -0800

    Char is not blittable in C# (#16)

--- TestComponent/TestRunner.cpp
+++ TestComponent/TestRunner.cpp
@@ -139,9 +139,9 @@ namespace winrt::TestComponent::implementation
         TEST_REQUIRE_N(L"Param", number, a == b && a == c); \
     }
 
-        TEST_GEN(13, Blittable, (Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'X', guid_of<ITests>() }));
-        TEST_GEN(14, NonBlittable, (NonBlittable{ false, L"WinRT", 1234 }));
-        TEST_GEN(15, Nested, (Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'X', guid_of<ITests>() }, { true, L"WinRT", 1234 } }));
+        TEST_GEN(13, Blittable, (Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<ITests>() }));
+        TEST_GEN(14, NonBlittable, (NonBlittable{ false, L'X', L"WinRT", 1234 }));
+        TEST_GEN(15, Nested, (Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<ITests>() }, { true, L'X', L"WinRT", 1234 } }));
 
 #undef TEST_GEN
 
@@ -200,9 +200,9 @@ namespace winrt::TestComponent::implementation
         TEST_GEN(10, double, 4.0f, 5.0f, 6.0f);
         TEST_GEN(11, char16_t, L'W', L'i', L'n');
         TEST_GEN(12, hstring, L"C++", L"C#", L"Rust");
-        TEST_GEN(13, Blittable, Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'X', guid_of<ITests>() }, Blittable{ 10, 20, 30, 40, -50, -60, -70, 80.0f, 90.0, L'Y', guid_of<IStringable>() }, Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'Z', guid_of<IInspectable>() });
-        TEST_GEN(14, NonBlittable, NonBlittable{ false, L"First", 123 }, NonBlittable{ true, L"Second", 456 }, NonBlittable{ false, L"Third", 789 });
-        TEST_GEN(15, Nested, Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'X', guid_of<ITests>() }, { false, L"First", 123 } }, Nested{ { 10, 20, 30, 40, -50, -60, -70, 80.0f, 90.0, L'Y', guid_of<IStringable>() }, { true, L"Second", 456 } }, Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, L'Z', guid_of<IInspectable>() }, { false, L"Third", 789 } });
+        TEST_GEN(13, Blittable, Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<ITests>() }, Blittable{ 10, 20, 30, 40, -50, -60, -70, 80.0f, 90.0, guid_of<IStringable>() }, Blittable{ 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<IInspectable>() });
+        TEST_GEN(14, NonBlittable, NonBlittable{ false, L'X', L"First", 123 }, NonBlittable{ true, L'Y', L"Second", 456 }, NonBlittable{ false, L'Z', L"Third", 789 });
+        TEST_GEN(15, Nested, Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<ITests>() }, { false, L'X', L"First", 123 } }, Nested{ { 10, 20, 30, 40, -50, -60, -70, 80.0f, 90.0, guid_of<IStringable>() }, { true, L'Y', L"Second", 456 } }, Nested{ { 1, 2, 3, 4, -5, -6, -7, 8.0f, 9.0, guid_of<IInspectable>() }, { false, L'Z', L"Third", 789 } });
 
 #undef TEST_GEN
 
