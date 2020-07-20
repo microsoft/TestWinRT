4f923ffd68418ee2ae2f1b38f2455e2a891b6796
UJJWAL CHADHA <ujchadha@microsoft.com>
2020-07-20 17:08:08 -0400

    Added benchmark classes for marshaling and reflection scenarios.

--- BenchmarkComponent/BenchmarkComponent.cpp
+++ BenchmarkComponent/BenchmarkComponent.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "BenchmarkComponent.h"
 #include "ClassWithMultipleInterfaces.g.cpp"
+#include "ClassWithMarshalingRoutines.g.cpp"
+#include "WrappedClass.g.cpp"
+
+using namespace winrt::Windows::Foundation;
 
 namespace winrt::BenchmarkComponent::implementation
 {
@@ -61,4 +65,63 @@ namespace winrt::BenchmarkComponent::implementation
     void ClassWithMultipleInterfaces::DefaultDoubleProperty(double val)
     {
     }
+
+    ClassWithMarshalingRoutines::ClassWithMarshalingRoutines()
+    {
+    }
+
+    hstring ClassWithMarshalingRoutines::DefaultStringProperty()
+    {
+        return hstring();
+    }
+
+    void ClassWithMarshalingRoutines::DefaultStringProperty(hstring val)
+    {
+    }
+    
+    Windows::Foundation::IInspectable ClassWithMarshalingRoutines::DefaultKeyValuePairProperty()
+    {
+        Windows::Foundation::Collections::ValueSet valueSet;
+        valueSet.Insert(hstring(L"key"), PropertyValue::CreateString(L"value"));
+        return valueSet.First().Current();
+    }
+
+    void ClassWithMarshalingRoutines::DefaultKeyValuePairProperty(Windows::Foundation::IInspectable val)
+    {
+    }
+
+    Windows::Foundation::IInspectable ClassWithMarshalingRoutines::DefaultNullableProperty()
+    {
+        return IReference<INT32>(123);
+    }
+
+    void ClassWithMarshalingRoutines::DefaultNullableProperty(Windows::Foundation::IInspectable val)
+    {
+    }
+
+    Windows::Foundation::IInspectable ClassWithMarshalingRoutines::DefaultArrayProperty()
+    {
+        int values[]{ 0, 42, 1729, -1 };
+        auto propertyValue = PropertyValue::CreateInt32Array(values).as<IPropertyValue>();
+        auto referenceArray = propertyValue.as<IReferenceArray<int>>();
+        return referenceArray;
+    }
+
+    void ClassWithMarshalingRoutines::DefaultArrayProperty(Windows::Foundation::IInspectable val)
+    {
+    }
+    
+    WrappedClass::WrappedClass()
+    {
+    }
+
+    int32_t WrappedClass::DefaultIntProperty()
+    {
+        return int32_t();
+    }
+
+    void WrappedClass::DefaultIntProperty(int32_t val)
+    {
+    }
+
 }
--- BenchmarkComponent/BenchmarkComponent.h
+++ BenchmarkComponent/BenchmarkComponent.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ClassWithMultipleInterfaces.g.h"
+#include "ClassWithMarshalingRoutines.g.h"
+#include "WrappedClass.g.h"
 
 namespace winrt::BenchmarkComponent::implementation
 {
@@ -24,11 +26,46 @@ namespace winrt::BenchmarkComponent::implementation
         void DefaultBoolProperty(bool val);
         void DefaultDoubleProperty(double val);
     };
+
+    struct WrappedClass : WrappedClassT<WrappedClass>
+    {
+        WrappedClass();
+
+        int32_t DefaultIntProperty();
+        void DefaultIntProperty(int32_t val);
+    };
+
+    struct ClassWithMarshalingRoutines : ClassWithMarshalingRoutinesT<ClassWithMarshalingRoutines>
+    {
+        ClassWithMarshalingRoutines();
+
+        hstring DefaultStringProperty();
+        void DefaultStringProperty(hstring val);
+
+        /*Windows::Foundation::IInspectable DefaultWrappedClassProperty();
+        void DefaultWrappedClassProperty(Windows::Foundation::IInspectable val);*/
+        Windows::Foundation::IInspectable DefaultKeyValuePairProperty();
+        void DefaultKeyValuePairProperty(Windows::Foundation::IInspectable val);
+
+        Windows::Foundation::IInspectable DefaultNullableProperty();
+        void DefaultNullableProperty(Windows::Foundation::IInspectable val);
+
+        Windows::Foundation::IInspectable DefaultArrayProperty();
+        void DefaultArrayProperty(Windows::Foundation::IInspectable val);
+    };
 }
 
 namespace winrt::BenchmarkComponent::factory_implementation
 {
     struct ClassWithMultipleInterfaces : ClassWithMultipleInterfacesT<ClassWithMultipleInterfaces, implementation::ClassWithMultipleInterfaces>
+    {
+    };
+
+    struct ClassWithMarshalingRoutines : ClassWithMarshalingRoutinesT<ClassWithMarshalingRoutines, implementation::ClassWithMarshalingRoutines>
+    {
+    };
+
+    struct WrappedClass : WrappedClassT<WrappedClass, implementation::WrappedClass>
     {
     };
 }
