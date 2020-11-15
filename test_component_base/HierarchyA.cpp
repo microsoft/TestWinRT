d44bccd47c6754dee589975e1406f9ac500900bb
Ujjwal Chadha <66971578+ujjwalchadha@users.noreply.github.com>
2020-11-15 13:35:01 -0500

    Added inheritance test projects

---
+++ test_component_base/HierarchyA.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "HierarchyA.h"
+#include "HierarchyA.g.cpp"
+
+namespace winrt::test_component_base::implementation
+{
+    HierarchyA::HierarchyA(hstring const& name)
+    {
+        throw hresult_not_implemented();
+    }
+    void HierarchyA::HierarchyA_Method()
+    {
+        //test_component_base::HierarchyA a = *this;
+        //assert(a);
+
+        //test_component_base::IHierarchyA ia = *this;
+        //assert(a);
+    }
+}
---
+++ test_component_derived/Nested.HierarchyC.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "Nested.HierarchyC.h"
+#include "Nested.HierarchyC.g.cpp"
+
+namespace winrt::test_component_derived::Nested::implementation
+{
+    HierarchyC::HierarchyC(hstring const& name)
+    {
+        throw hresult_not_implemented();
+    }
+    void HierarchyC::HierarchyC_Method()
+    {
+        throw hresult_not_implemented();
+    }
+}
