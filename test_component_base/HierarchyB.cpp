5024e7d7f3d9a0062b8d171cd83752248489c36d
Ujjwal Chadha <66971578+ujjwalchadha@users.noreply.github.com>
2020-11-20 14:06:35 -0500

    Ported inheritance test projects from CppWinRT (#40)
    * Added inheritance test projects
    
    * Ported inheritance tests from CppWinRT
    
    * Update nuspec fix dll path
    
    * Updated CppWinRT to latest
    
    * Delete test_component_derived.log
    
    Co-authored-by: Kenny Kerr <kekerr@microsoft.com>

---
+++ test_component_base/HierarchyB.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "HierarchyB.h"
+#include "HierarchyB.g.cpp"
+
+namespace winrt::test_component_base::implementation
+{
+    HierarchyB::HierarchyB(hstring const& name)
+    {
+        throw hresult_not_implemented();
+    }
+    void HierarchyB::HierarchyB_Method()
+    {
+        throw hresult_not_implemented();
+    }
+}
