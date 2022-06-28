fb02a8e4a856f353e912bbed26c1ba7d7700d349
Manodasan Wignarajah <mawign@microsoft.com>
2022-06-27 17:02:24 -0700

    Add tests to exercise composable scenarios using parent class

--- test_component_derived/Nested.HierarchyC.cpp
+++ test_component_derived/Nested.HierarchyC.cpp
@@ -18,4 +18,28 @@ namespace winrt::test_component_derived::Nested::implementation
     {
         return L"HierarchyC.HierarchyC_Method";
     }
+
+    winrt::test_component_derived::Nested::HierarchyC HierarchyC::CreateDerivedHierarchyD()
+    {
+        return HierarchyD();
+    }
+
+    Windows::Foundation::IInspectable HierarchyC::CreateDerivedHierarchyDAsObject()
+    {
+        return HierarchyD();
+    }
+
+    Windows::Foundation::Collections::IVector<winrt::test_component_derived::Nested::HierarchyC> HierarchyC::CreateDerivedHierarchyDList()
+    {
+        Windows::Foundation::Collections::IVector<winrt::test_component_derived::Nested::HierarchyC> list = 
+            single_threaded_vector<winrt::test_component_derived::Nested::HierarchyC>({ HierarchyD(), HierarchyD(), HierarchyD() });
+        return list;
+    }
+
+    Windows::Foundation::Collections::IVector<Windows::Foundation::IInspectable> HierarchyC::CreateDerivedHierarchyDAsObjectList()
+    {
+        Windows::Foundation::Collections::IVector<Windows::Foundation::IInspectable> list =
+            single_threaded_vector<Windows::Foundation::IInspectable>({ HierarchyD(), HierarchyD(), HierarchyD() });
+        return list;
+    }
 }
